4589d740c3b491ca37219bd84ec52f916d367585
FrotaLucas <lucasdiasfrota@gmail.com>
2025-03-29 23:38:38 +0100

    new class WheaterDataMoldel

---
+++ WheaterApi/Model/WheaterDataModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WheaterApi.Model
+{
+    public class WheaterDataModel
+    {
+       public List<string> Time {  get; set; }
+
+        [JsonPropertyName("temperature_2m")]
+        public List<double> Temperature { get; set; }
+
+    }
+}
