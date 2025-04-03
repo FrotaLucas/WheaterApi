0348a37d364ba9a5beff3d31ad444f02aaa6752f
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-03 18:06:03 -0300

    new class Gemetry

--- WheaterApi/Model/Geometry.cs
+++ WheaterApi/Model/Geometry.cs
@@ -1,9 +1,13 @@
-﻿namespace WheaterApi.Model
+﻿using System.Text.Json.Serialization;
+
+namespace WheaterApi.Model
 {
     public class Geometry
     {
+        [JsonPropertyName("latitude")]
         public string Latitude { get; set; } = string.Empty;
 
+        [JsonPropertyName("longitude")]
         public string Longitude { get; set; } = string.Empty;
     }
 }
