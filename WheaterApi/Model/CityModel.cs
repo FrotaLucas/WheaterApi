39567028610b06b78ecf192b5c5f0030bce19f17
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-03 18:02:57 -0300

    new class city mode

---
+++ WheaterApi/Model/CityModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WheaterApi.Model
+{
+    public class CityModel
+    {
+        [JsonPropertyName("geometry")]
+       public Geometry Geometry { get; set; }
+
+        [JsonPropertyName("timezone")]
+       public Timezone Timezone { get; set; }
+
+    }
+}
