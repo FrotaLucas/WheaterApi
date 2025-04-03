3a4e15c744ea2d36dde9879f3b5a747198746e43
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-03 17:57:19 -0300

    new classe

---
+++ WheaterApi/Model/Geometry.cs
@@ -0,0 +1,9 @@
+﻿namespace WheaterApi.Model
+{
+    public class Geometry
+    {
+        public string Latitude { get; set; } = string.Empty;
+
+        public string Longitude { get; set; } = string.Empty;
+    }
+}
