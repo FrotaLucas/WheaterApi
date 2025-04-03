b99c980df269956e79e3195289ba91da8dd99b7e
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-03 15:07:21 -0300

    adapt method to test without redis

--- WheaterApi/Services/WheaterService.cs
+++ WheaterApi/Services/WheaterService.cs
@@ -14,32 +14,36 @@ namespace WheaterApi.Services
             _cache = cache;
         }
 
-        public async Task<WheaterModel> getWheaterData(double latitude, double longitude)
+        public async Task<WheaterModel> getWheaterData(double latitude, double longitude, string timezone)
         {
-            string url = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true";
+            string url = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m&timezone=Europe%2F{timezone}&forecast_days=1";
+            //"https://api.open-meteo.com/v1/forecast?latitude=48.1374&longitude=11.5755&hourly=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1"
 
             string cacheKey = $"{latitude}-{longitude}";
-            var cacheData = await _cache.GetStringAsync(cacheKey) ;
+            //var cacheData = await _cache.GetStringAsync(cacheKey) ;
 
-            if(!string.IsNullOrEmpty(cacheData))
-            {
-                //transform Byte data format into json ?
-                var json = JsonSerializer.Deserialize<WheaterModel>(cacheData) ;
-                return json;
-            }
+            //if(!string.IsNullOrEmpty(cacheData))
+            //{
+            //    //transform Byte data format into json ?
+            //    var json = JsonSerializer.Deserialize<WheaterModel>(cacheData) ;
+            //    return json;
+            //}
 
             var responseApi = await _httpClient.GetFromJsonAsync<WheaterModel>(url);
 
             //set cache for 10 min
-            var options = new DistributedCacheEntryOptions()
-                .SetAbsoluteExpiration(TimeSpan.FromMinutes(10));
+            //var options = new DistributedCacheEntryOptions()
+            //    .SetAbsoluteExpiration(TimeSpan.FromMinutes(10));
 
-            _cache.SetStringAsync(cacheKey, JsonSerializer.Serialize(responseApi), options);
+            //_cache.SetStringAsync(cacheKey, JsonSerializer.Serialize(responseApi), options);
 
             return new WheaterModel
             {
                 Latitude = responseApi.Latitude,
-                Longitude = responseApi.Longitude
+                Longitude = responseApi.Longitude,
+                TimeZone = responseApi.TimeZone,
+                TemperatureDate = responseApi.TemperatureDate,
+                
             };
 
 
