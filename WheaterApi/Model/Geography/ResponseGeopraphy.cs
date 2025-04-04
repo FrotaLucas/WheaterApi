7041a5537117048e146492d820a50f1937c557bb
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-04 15:14:15 -0300

    add new class  responseGeography

---
+++ WheaterApi/Model/Geography/ResponseGeopraphy.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WheaterApi.Model.Geography
+{
+    public class ResponseGeopraphy
+    {
+        [JsonPropertyName("results")]
+        public List<GeographyData> results { get; set; }
+
+    }
+}
