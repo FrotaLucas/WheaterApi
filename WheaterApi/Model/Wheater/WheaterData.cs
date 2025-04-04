9ac3a21d21e67d0f3bf12c5a6b57814cec075f1c
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-04 15:16:16 -0300

    add new class WheaterData

---
+++ WheaterApi/Model/Wheater/WheaterData.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WheaterApi.Model.Wheater
+{
+    public class WheaterData
+    {
+        public List<string> Time { get; set; }
+
+        [JsonPropertyName("temperature_2m")]
+        public List<double> Temperature { get; set; }
+
+    }
+}
