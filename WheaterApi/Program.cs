52ac1888093be9098c370950a4b1d721e7a00ca3
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-04 17:02:42 -0300

    Ports HTTPS HTTP

--- WheaterApi/Program.cs
+++ WheaterApi/Program.cs
@@ -2,40 +2,41 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Net.Http.Json;
 using WheaterApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Força a aplicação a rodar na porta 4000
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // HTTP
+    //options.ListenAnyIP(4000); 
+    // HTTPS see launchSettings
+    options.ListenAnyIP(4001, listenOptions => listenOptions.UseHttps()); 
+});
+
 // Redis Config
 builder.Services.AddStackExchangeRedisCache(options =>
 {
-    // Se o Redis estiver rodando localmente
-    options.Configuration = "localhost";  
-    //se quiser adicionar uma palavra extra a chave redis armazenada no cache
-    //options.InstanceName = "WheaterCache:";
+    options.Configuration = "localhost";
 });
 
-// service to consume api  open-meteo
+// Service para consumir a API open-meteo
 builder.Services.AddHttpClient<WheaterService>();
 
-// add controllers
+// Add controllers e Swagger
 builder.Services.AddControllers();
-
-//swaggerDokumentatio
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 var app = builder.Build();
 
-if(app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(); // por padrão ele já usa /swagger como rota
 }
 
-//app.MapGet("/", () => "API wirking!");
 app.MapControllers();
 
 app.Run();
