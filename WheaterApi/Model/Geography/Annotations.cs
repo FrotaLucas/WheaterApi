03b636c3e3970b1828c7a8058694cfd692f8a167
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-04 15:14:39 -0300

    add new class Annotation

---
+++ WheaterApi/Model/Geography/Annotations.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WheaterApi.Model.Geography
+{
+    public class Annotations
+    {
+        [JsonPropertyName("timezone")]
+        public Timezone Timezone { get; set; }
+    }
+}
