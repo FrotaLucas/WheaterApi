0c54015b9b10336ca5a6ed7fab1612c70c70694c
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-04 15:15:02 -0300

    add new class GeographyData

---
+++ WheaterApi/Model/Geography/GeographyData.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WheaterApi.Model.Geography
+{
+    public class GeographyData
+    {
+        [JsonPropertyName("geometry")]
+        public Geometry Geometry { get; set; }
+
+        [JsonPropertyName("annotations")]
+        public Annotations Annotations { get; set; }
+
+    }
+}
