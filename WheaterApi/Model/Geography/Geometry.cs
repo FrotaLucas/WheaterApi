9499886bbda526e62e38c26715de56f164195d49
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-04 15:15:15 -0300

    add new class Geometry

---
+++ WheaterApi/Model/Geography/Geometry.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WheaterApi.Model.Geography
+{
+    public class Geometry
+    {
+        [JsonPropertyName("lat")]
+        public double Latitude { get; set; }
+
+        [JsonPropertyName("lng")]
+        public double Longitude { get; set; }
+    }
+}
