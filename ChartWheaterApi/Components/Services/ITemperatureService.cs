69ea809dcbcfd8dfe6a8d339457b00f0d1deaca9
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-05 15:18:05 -0300

    Service Temperature

---
+++ ChartWheaterApi/Components/Services/ITemperatureService.cs
@@ -0,0 +1,10 @@
+﻿using WheaterApi.Model.Wheater;
+
+namespace ChartWheaterApi.Components.Services
+{
+    public interface ITemperatureService
+    {
+        Task<ResponseWheater> getTemperature();
+        
+    }
+}
---
+++ ChartWheaterApi/Components/Services/TemperatureService.cs
@@ -0,0 +1,25 @@
+﻿using WheaterApi.Model.Wheater;
+
+namespace ChartWheaterApi.Components.Services
+{
+    public class TemperatureService : ITemperatureService
+    {
+        public TemperatureService() { }
+
+        public async Task<ResponseWheater> getTemperature()
+        {
+
+
+            return new ResponseWheater()
+            {
+                Latitude = 0,
+                Longitude = 0,
+                TimeZone = "America",
+                TemperatureDate = new WheaterData()
+                {
+                    Temperature = [25,23,29,4,23,23]
+                }
+            };
+        }
+    }
+}
