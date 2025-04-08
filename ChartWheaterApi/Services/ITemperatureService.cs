ce9f82818d875e3ab9d84c41d40c6b54037251e0
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-08 16:37:27 -0300

    changes in TemperatureService

--- ChartWheaterApi/Services/ITemperatureService.cs
+++ ChartWheaterApi/Services/ITemperatureService.cs
@@ -1,6 +1,14 @@
-﻿namespace ChartWheaterApi.Services
+﻿using Wheater.Shared.Model.Wheater;
+
+namespace ChartWheaterApi.Services
 {
     public interface ITemperatureService
     {
+        Task UpdateApi(string city = "Berlin");
+        Task<ResponseWheater> getTemp(string city);
+
+        event Action TemperatureChanged;
+        ResponseWheater Data { get; set; }
+
     }
 }
--- ChartWheaterApi/Services/TemperatureService.cs
+++ ChartWheaterApi/Services/TemperatureService.cs
@@ -1,6 +1,47 @@
-﻿namespace ChartWheaterApi.Services
+﻿using System.Net.Http.Json;
+using Wheater.Shared.Model.Wheater;
+
+namespace ChartWheaterApi.Services
 {
-    public class TemperatureService
+    public class TemperatureService : ITemperatureService
     {
+        private readonly HttpClient _httpClient;
+        public TemperatureService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public ResponseWheater Data { get; set; } = new ResponseWheater();
+
+        public event Action TemperatureChanged;
+
+        public async Task<ResponseWheater> getTemp(string city)
+        {
+            var result = await _httpClient.GetFromJsonAsync<ResponseWheater>($"api/Wheater?city={city}");
+            //http://localhost:5000/api/Wheater?city=coroaci
+
+            Data = result;
+
+            return result;//talvez nao preciso restornar result, pq a atualizacao dos dados eh feito pelo Evento no DOM
+        }
+        public async Task UpdateApi(string city)
+        {
+            var result = await getTemp(city);
+
+            //var response = new ResponseWheater()
+            //{
+            //    Latitude = result.Latitude,
+            //    Longitude = result.Longitude,
+            //    TimeZone = result.TimeZone,
+            //    TemperatureDate = result.TemperatureDate,
+            //    //TemperatureDate = new WheaterData()
+            //    //{
+            //    //    Temperature = [25,23,29,4,23,23]
+            //    //}
+            //};
+
+            TemperatureChanged.Invoke();
+        }
+
     }
 }
