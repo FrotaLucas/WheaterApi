c3ecac48fd4bb80744fbe9002c58afdad19a1ceb
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-09 20:44:55 -0300

    set up configuration of Synfusion

--- ChartWheaterApi/Program.cs
+++ ChartWheaterApi/Program.cs
@@ -2,11 +2,16 @@ using ChartWheaterApi;
 using ChartWheaterApi.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSyncfusionBlazor();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(
+    "MzgwNzMyMkAzMjM5MmUzMDJlMzAzYjMyMzkzYmNiRFkvMnd4Yjh1T2hHWkZtcHR2UldyY1R4cDh5WkFKNStOdEtwS2hmT1U9"
+);
 
 //investigar pq essa 1 opcao nao eh valida. Esta igual projeto Ecommerce
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
