05acb2551e9df17b6e702390772fd130509945a5
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-10 12:10:18 -0300

    add new class CharTemperatureData

---
+++ Wheater.Shared/Model/Chart/ChartTemperatureData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wheater.Shared.Model.Chart
+{
+    public class ChartTemperatureData
+    {
+        public string Time { get; set; } = string.Empty;
+        public double Temps { get; set; }
+    }
+}
