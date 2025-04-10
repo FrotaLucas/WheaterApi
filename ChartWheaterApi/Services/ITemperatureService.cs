a677c41728e8a66e8dd6b22f191b3297633d64c7
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-09 21:33:24 -0300

    new variable chartData into ITemperature

--- ChartWheaterApi/Services/ITemperatureService.cs
+++ ChartWheaterApi/Services/ITemperatureService.cs
@@ -1,14 +1,19 @@
-﻿using Wheater.Shared.Model.Wheater;
+﻿using Wheater.Shared.Model.Chart;
+using Wheater.Shared.Model.Wheater;
 
 namespace ChartWheaterApi.Services
 {
     public interface ITemperatureService
     {
         Task UpdateApi(string city = "Berlin");
+        
         Task<ResponseWheater> getTemp(string city);
 
         event Action TemperatureChanged;
+        
         ResponseWheater Data { get; set; }
+
+        ChartData chartData { get; set; }
 
     }
 }
