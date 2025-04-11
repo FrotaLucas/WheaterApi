94ad377f2e4dc1281bf4fc62e295b97165c8018e
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-11 17:20:51 -0300

    clean commments

--- ChartWheaterApi/Program.cs
+++ ChartWheaterApi/Program.cs
@@ -14,10 +14,6 @@ var Mykey = builder.Configuration.GetConnectionString("MySyncfusionLisence");
 builder.Services.AddSyncfusionBlazor();
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(Mykey);
 
-//investigar pq essa 1 opcao nao eh valida. Esta igual projeto Ecommerce
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddScoped<ITemperatureService, TemperatureService>();
-
 builder.Services.AddHttpClient<IWheaterService, WheaterService>(client =>
 {
     client.BaseAddress = new Uri("http://localhost:5000/");
